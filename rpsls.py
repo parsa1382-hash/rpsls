9e211ad122eafba2327bb986748050b5481dec98
parsa1382-hash <parsa3847@gmail.com>
2020-08-12 11:59:14 +0430

    adding

--- rpsls.py
+++ rpsls.py
@@ -2,36 +2,40 @@ import random
 
 def main():
 
-    
     #make a class to define a player & play objects
-    class player:
+    class Player:
+
         def __init__(self):
-            self.moves=[0,1,2]
-                      #[r,p,s,l,s]
-            self.wingame=0 #--> count how many player won
+            self.moves = [0,1,2]
+                        #[r,p,s]
+            self.wingame = 0 #--> count how many player won
             
             #make a matrix of win & lose graph
-            self.win_matrix=[[0,1],[1,2],[2,0]]        
+            self.win_matrix = [[0,1],[1,2],[2,0]]        
 
         def win(self):
             self.wingame += 1
+
         def play(self):
             return random.choice(self.moves)
 
 
-    class rpsls_player(player):
+    class Rpsls_Player(player):
+
         def __init__(self):
             player.__init__(self)
             
             #make a matrix of win & lose graph
-            self.win_matrix=[[0, 1], [0, 3], [1, 2], [1, 4], [2, 3]
+            self.win_matrix = [[0, 1], [0, 3], [1, 2], [1, 4], [2, 3]
                             , [2, 0], [3, 4], [3, 1], [4, 0], [4, 2]]
             #[0,1,2,3,4,] mean --> [Rock, Paper, Scissors, Lizard, Spock]
 
 
+
+
     def game(a,b):
-        p=[a.play(),b.play()]
-        if p[0]==p[1]:
+        p = [a.play(),b.play()]
+        if p[0] == p[1]:
             pass 
 
         elif p in a.win_matrix:
@@ -41,25 +45,29 @@ def main():
 
 
 #normal game
-    a=player()
-    b=player()
+    a = Player()
+    b = Player()
+
     while True:
         if (a.wingame == 100) or (b.wingame == 100):
             break
         game(a,b)
     print("normal game:\na wins: {}\nb wins: {}".format(a.wingame,b.wingame))
-
     print("\n\n")
 
 
-#rpsls game
-    a2=rpsls_player()
-    b2=rpsls_player()
+    #rpsls game
+    a2=Rpsls_Player()
+    b2=Rpsls_player()
+    
     while True:
         if (a2.wingame == 100) or (b2.wingame == 100):
             break
+
         game(a2,b2)
     print("rpsls game:\na2 wins: {}\nb2 wins: {}".format(a2.wingame,b2.wingame))
+
+
 
 if __name__=='__main__':
     main()
